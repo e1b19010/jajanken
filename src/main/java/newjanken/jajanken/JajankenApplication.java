0014066cac3c7cdc05d5f3c4eef4b13dda09044c
e1b19010 <e1b19010@oit.ac.jp>
2021-11-11 21:32:26 +0900

    リポジトリの初期化

---
+++ src/main/java/newjanken/jajanken/JajankenApplication.java
@@ -0,0 +1,13 @@
+package newjanken.jajanken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JajankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JajankenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/newjanken/jajanken/JajankenApplicationTests.java
@@ -0,0 +1,13 @@
+package newjanken.jajanken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JajankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
